47ed8f7cadb9a68481adbdb2957dc1a0e15bdf0f
srinivaspaluvayi <72265945+srinivaspaluvayi@users.noreply.github.com>
2021-05-21 14:40:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from flask import request
 import requests_cache
 from twilio.rest import Client
 
-account_sid = 'AC5615fae9f752556039e56b0a48640877'
-auth_token = '6d2b911db46e2a742816d0db7796a7ef'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 app = Flask(__name__, static_url_path='/static')
 
@@ -35,7 +35,7 @@ def login_registration_dtls():
     travel_pass = ((cnt / pop) * 100)
     if travel_pass < 30 and request.method == 'POST':
         status = 'CONFIRMED'
-        client.messages.create(to="whatsapp:+919381475028",
+        client.messages.create(to="whatsapp:",
                                from_="whatsapp:+14155238886",
                                body="Hello " + " " + full_name + " " + "Your Travel From " + " " + source_dt + " " +
                                     "To" + " " + destination_dt + " " + "Has" + " " + status + " On" + " " + date + " "
@@ -46,7 +46,7 @@ def login_registration_dtls():
                                var8=date, var9=status)
     else:
         status = 'Not Confirmed'
-        client.messages.create(to="whatsapp:+919381475028",
+        client.messages.create(to="whatsapp:",
                                from_="whatsapp:+14155238886",
                                body="Hello " + " " + full_name + "  " + "your travel from" + source_dt + " " + "To" +
                                     " " + destination_dt + " "
@@ -57,4 +57,4 @@ def login_registration_dtls():
 
 
 if __name__ == "__main__":
-    app.run(port=9001, debug=True)
+    app.run(port=4000, debug=True)
